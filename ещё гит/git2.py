6cc4a7dc6ce9e715e4a6ff614ac1ce53ff28378c
mge410 <vov.voron2005@mail.ru>
2022-02-03 11:17:27 +0300

    Доработка

--- ещё гит/git2.py
+++ ещё гит/git2.py
@@ -1,17 +1,19 @@
 import sys
 import random
 
-from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtGui import QPainter, QColor, QPen
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+from UI import Ui_MainWindow
 
-class MyWidget(QMainWindow):
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
-        self.color = "Yellow"
+        self.color = ['Red', 'Orange', 'Yellow', 'Green', 'Cyan',
+                       'Blue', 'Magenta', 'Purple', 'Brown', 'Black']
         self.flag = False
 
     def run(self):
@@ -24,7 +26,7 @@ class MyWidget(QMainWindow):
             pen.setWidth(3)
             painter = QPainter()
             painter.begin(self)
-            painter.setBrush(QColor(self.color))
+            painter.setBrush(QColor(random.choice(self.color)))
             radius = random.randint(1, 600)
             painter.drawEllipse(random.randint(1, self.width() - radius), random.randint(1, self.height() - radius), radius, radius)
             painter.end()
